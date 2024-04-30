b135d5a2c38eb24042ccf14e3bec65527ecf5cfb
Christian Rivero <christianriveroarnez@gmail.com>
2024-04-30 08:25:03 -0400

    notis get correctly

--- lib/firebase_messaging_service.dart
+++ lib/firebase_messaging_service.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class FirebaseMessagingService {
   FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
-  void setupFirebaseMessaging() {
+  void setupFirebaseMessaging(
+      Function(String, String) onMessageReceived) async {
+    print("Hi");
+    final notificationSettings =
+        await FirebaseMessaging.instance.requestPermission(provisional: true);
+    print("not status");
+    print(notificationSettings.alert);
     _firebaseMessaging.getToken().then((token) {
       print('Firebase Token: $token');
     });
 
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print(message.data);
       print('Received message: ${message.notification?.title}');
+      print('Received message: ${message.notification?.body}');
+      onMessageReceived(
+          message.notification?.title ?? "", message.notification?.body ?? "");
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:exam_mobile_2/firebase_messaging_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'firebase_messaging_service.dart'; // Import the Firebase Messaging service
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,95 +16,65 @@ void main() async {
   } catch (e) {
     print("Failed to initialize Firebase: $e");
   }
-  try {
-    FirebaseMessagingService().setupFirebaseMessaging();
-  } catch (e) {
-    print("Failed to setup Firebase Messaging: $e");
-  }
-  runApp(const MyApp());
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    initializeNotifications(); // Initialize notifications
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo Home Page'),
+        ),
+        body: Center(
+          child: Text('Check for notifications!'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+
+  void initializeNotifications() {
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    try {
+      FirebaseMessagingService().setupFirebaseMessaging(displayNotification);
+    } catch (e) {
+      print("Failed to initialize Firebase Messaging: $e");
+    }
+  }
+
+  void displayNotification(String? title, String? body) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    var platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0, // Notification ID
+      title ?? '', // Notification title
+      body ?? '', // Notification body
+      platformChannelSpecifics,
+      payload: 'notification_payload',
     );
   }
 }
